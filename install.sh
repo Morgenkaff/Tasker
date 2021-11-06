be7527f060021f380f71b527b9f70fe3b879b45a
Morgenkaff <mail@morgenkaff.dk>
2021-11-06 19:16:16 +0100

    Updated install script to install addTasker.desktop too.

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Set install directory
-install_dir="$HOME/.local/bin"
+install_dir_bin="$HOME/.local/bin"
+install_dir_service_menu="$HOME/.local/share/kservices5/"
 
 # Set directory for config file
 config_dir="$HOME/.config/tasker"
@@ -10,15 +11,19 @@ config_dir="$HOME/.config/tasker"
 chmod +x scan_dir.py
 
 # Check if directory exist, else create them
-if [[ ! -d "$install_dir" ]]; then
-    mkdir "$install_dir"
+if [[ ! -d "$install_dir_bin" ]]; then
+    mkdir "$install_dir_bin"
+fi
+if [[ ! -d "$install_dir_service_menu" ]]; then
+    mkdir "$install_dir_service_menu"
 fi
 if [[ ! -d "$config_dir" ]]; then
     mkdir "$config_dir"
 fi
 
 # Then, copy the files
-cp scan_dir.py "$install_dir"/scan_dir
+cp scan_dir.py "$install_dir_bin"/scan_dir
+cp addTask.desktop "$install_dir_service_menu"/addTask.desktop
 cp config.yml "$config_dir"/
 
 # Add to cron:
@@ -26,7 +31,7 @@ cp config.yml "$config_dir"/
 crontab -l > mycron
 #echo new cron into cron file
 #echo "@daily scan_dir" >> mycron
-echo "* * * * * python3 $install_dir/scan_dir -v" >> mycron
+echo "* * * * * python3 $install_dir_bin/scan_dir -v" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
